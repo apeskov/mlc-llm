b7cc8a8b8d754ce16a3828ff9a03e696dce1a8d3
blabla <blabla@email.com>
2023-06-23 11:38:00 +0000

    Evaluate promp-answer
    Signed-off-by: blabla <blabla@email.com>

--- tests/evaluate.py
+++ tests/evaluate.py
@@ -3,6 +3,7 @@
 import argparse
 import os
 import time
+import random
 from typing import List, Tuple
 
 import numpy as np
@@ -78,6 +79,96 @@ def print_as_table(sorted_list: List[Tuple[str, Tuple[float, int]]]):
         )
     print("Total time: {:.4f} ms".format(total_time))
     print()
+
+
+def answer_prompt(args) -> None:
+    device = tvm.device(args.device_name)
+    const_params = utils.load_params(args.artifact_path, device)
+    ex = tvm.runtime.load_module(
+        os.path.join(
+            args.artifact_path,
+            f"{args.model}-{args.quantization.name}-{args.device_name}.so",
+        )
+    )
+    vm = relax.VirtualMachine(ex, device)
+    
+    top_p = 0.95
+    temperature = 0.8
+    
+    # prompt = args.prompt
+    # prompt = "2 plus 2"
+    # prompt = "Write the introduction to a steamy romance novel"
+    # prompt = "Lovely poema with flowers"
+    prompt = "What is the capital of Russia?"
+
+    full_prompt = f"Below is an instruction that describes a task. Write a response that appropriately completes the request.\n\n\n\n" \
+                  f"### Instruction:\n{prompt}\n\n" \
+                  f"### Response:\n"
+
+    temperature_arr = tvm.nd.empty({}, dtype="float32", device=device)
+    temperature_arr.copyfrom(np.array(temperature, dtype="float32"))
+
+    sample_top_p_from_prob = tvm.get_global_func("vm.builtin.sample_top_p_from_prob")
+
+    tokenizer = AutoTokenizer.from_pretrained(
+        os.path.join(args.artifact_path, "params"), trust_remote_code=True
+    )
+
+    print("Tokenizing...")
+    inputs_np = tokenizer(full_prompt, return_tensors="pt").input_ids.to(torch.int32).numpy()
+    inputs = tvm.nd.array(inputs_np, device)
+
+    first_sampled_token = tvm.nd.array(np.array([[6234]]).astype("int32"), device)
+    seq_len_shape = tvm.runtime.ShapeTuple([inputs.shape[1]])
+    second_seq_len_shape = tvm.runtime.ShapeTuple([inputs.shape[1] + 1])
+    
+    # Warmup
+    kv_caches = vm["create_kv_cache"]()
+    logits, kv_caches = vm["prefill"](inputs, seq_len_shape, kv_caches, const_params)
+    logits, kv_caches = vm["decode"](
+        first_sampled_token, second_seq_len_shape, kv_caches, const_params
+    )
+
+    # Inference
+    next_token_nd = tvm.nd.array(np.array([[6234]]).astype("int32"), device)
+    kv_caches = vm["create_kv_cache"]()
+    cur_seq_len = inputs.shape[1]
+    answer_tokens = []
+    
+    print("Running inference...")
+    
+    # from torch.profiler import profile, ProfilerActivity
+    # with profile(activities=[ProfilerActivity.CUDA], with_modules=False, with_stack=True) as prof:
+
+    logits, kv_caches = vm["prefill"](inputs, seq_len_shape, kv_caches, const_params)
+    prob = vm["softmax_with_temperature"](logits, temperature_arr)
+    next_token = sample_top_p_from_prob(prob, top_p, random.uniform(0, 1))
+    
+    answer_tokens.append(next_token)
+    
+    for _ in range(100):
+        next_token_nd.copyfrom(np.array([[next_token]], dtype="float32"))
+        cur_seq_len += 1
+        cur_seq_len_shape = tvm.runtime.ShapeTuple([cur_seq_len])
+        
+        second_seq_len_shape
+        logits, kv_caches = vm["decode"](
+            next_token_nd, cur_seq_len_shape, kv_caches, const_params
+        )
+        prob = vm["softmax_with_temperature"](logits, temperature_arr)
+        next_token = sample_top_p_from_prob(prob, top_p, random.uniform(0, 1))
+        next_token_text = tokenizer.decode(next_token) 
+
+        if next_token == 2 or next_token_text == "### End":
+            break
+        else: 
+            answer_tokens.append(next_token)
+    
+    # prof.export_chrome_trace("./trace_llm_tvm.json")
+
+    answer_text = tokenizer.decode(answer_tokens) 
+    print("Question: ", prompt)
+    print("Answer: ", answer_text)
 
 
 def deploy_to_pipeline(args) -> None:
@@ -165,4 +256,5 @@ def deploy_to_pipeline(args) -> None:
 
 if __name__ == "__main__":
     ARGS = _parse_args()
-    deploy_to_pipeline(ARGS)
+    # deploy_to_pipeline(ARGS)
+    answer_prompt(ARGS)
