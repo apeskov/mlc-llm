6274159e3867ce2945d967f14d9a6c1fcb740ac7
Bla bla <blabla@email.com>
2023-07-24 11:41:57 +0000

    Use hardcoded  dispatcher for dolly q4fp16
    Signed-off-by: Bla bla <blabla@email.com>

--- mlc_llm/dispatch/gpt_neox/__init__.py
+++ mlc_llm/dispatch/gpt_neox/__init__.py
@@ -1,6 +1,9 @@
 def lookup(func):
-    from . import dolly_v2_3b, redpajama_incite_chat_3b_v1, redpajama_q4f32
+    from . import dolly_v2_3b, redpajama_incite_chat_3b_v1, redpajama_q4f32, dolly_v2_12b_q4fp16
 
+    ret = dolly_v2_12b_q4fp16.lookup(func)
+    if ret is not None:
+        return ret
     ret = dolly_v2_3b.lookup(func)
     if ret is not None:
         return ret
